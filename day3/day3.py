aec3ead34b10e567a35c1e06a1b61c7b8873012d
feverbrew <33206171+feverbrew@users.noreply.github.com>
2021-12-20 14:35:57 -0500

    day3 formatting fun

--- day3/day3.py
+++ day3/day3.py
@@ -1,5 +1,7 @@
+from os import sep
 import time
 from typing import List
+import random
 
 def binary_to_decimal(binary: str) -> int:
     decimal = 0
@@ -9,21 +11,29 @@ def binary_to_decimal(binary: str) -> int:
     return decimal
 
 def run_diagnostics(input_file: str) -> int:
-    print('Now running system diagnostics...', flush=True)
+    print(f'\033[38;2;20;20;200m\033[48;2;255;255;255mNow running system diagnostics...\033[0m', flush=True)
     time.sleep(.5)
     with open(input_file) as input:
         diagnostic_feed = [line.strip() for line in input]
         line_length = len(diagnostic_feed[0])
         gamma_counts = [0] * line_length
         # Figure out how common things are
+        index = 0
+        width = 20
         for row in diagnostic_feed:
             list(row)
             for i in range(line_length):
                 if row[i] == '1':
-                    print('|*|',end='', flush=True)
+                    # print('|*|',end='', flush=True)
                     gamma_counts[i] += 1
-                else:
-                    print('| |',end='', flush=True)
+                # else:
+                #     print('| |',end='', flush=True)
+            index += 1
+            percent = 100 * float(index) / float(len(diagnostic_feed))
+            left = width * index // len(diagnostic_feed)
+            right = width - left
+            print('\r[', '#'*left, ' '*right, ']', f' {percent:.0f}%', flush=True, end='', sep='')
+            percent += 1
             time.sleep(.01)
 
     # Construct the resulting binaries
@@ -40,7 +50,7 @@ def run_diagnostics(input_file: str) -> int:
         else:
             epsilon_binary += '1'
 
-    print(f'\nSystem analyzed, calculating status code... \nWarning: errors {epsilon_binary} and {gamma_binary} have been detected!', flush=True)
+    print(f'\nSystem analyzed, calculating status code... \n\033[31;5mWarning:\033[0m errors \033[38;2;200;20;20m{epsilon_binary}\033[0m and \033[38;2;200;20;20m{gamma_binary}\033[0m have been detected!', flush=True)
 
     # Turn the binaries into decimal
     gamma_decimal = binary_to_decimal(gamma_binary)
@@ -48,7 +58,7 @@ def run_diagnostics(input_file: str) -> int:
 
     diagnostic_product = gamma_decimal * epsilon_decimal
 
-    print(f'Status code {diagnostic_product}.\nPlease check the owner\'s manual for more details.')
+    print(f'Status code {diagnostic_product}.\nPlease check the \033[38;2;20;230;20mowner\'s manual\033[0m for more details.')
 
     return diagnostic_product
 
@@ -60,8 +70,10 @@ def run_life_support_diagnostics(input_file: str) -> int:
 
     print(f'\nOxygen generator rating:', flush=True)
     oxygen_gen_rating = life_support_system_rating(diagnostic_feed, True)
+    print(' \033[92mok\033[0m')
     print(f'\nCO2 scrubber rating:', flush=True)
     CO2_scrubber_rating = life_support_system_rating(diagnostic_feed, False)
+    print(' \033[31mcheck system\033[0m')
 
     # Turn the binaries into decimal
     oxygen_decimal = binary_to_decimal(oxygen_gen_rating)
@@ -69,7 +81,15 @@ def run_life_support_diagnostics(input_file: str) -> int:
 
     life_support_rating = oxygen_decimal * CO2_decimal
 
-    print(f'\nLife support rating: {life_support_rating}')
+    print('\n\nLife support rating:')
+
+    for digit in str(life_support_rating):
+        for i in [random.randint(0,9) for j in range(10)]:
+            print(f'\b{i}', end='', flush=True)
+            time.sleep(.1)
+        print(f'\b{digit} ', end='', flush=True)
+        time.sleep(.1)
+    print(' Normal')
 
 def life_support_system_rating(diagnostic_feed: List, most: bool):
     # Get the system rating
